913c2ecbe71f8930dee6d4c0612682ff674e215b
Moritz Brückner <mail@moritz-brueckner.de>
2021-05-22 18:04:03 +0200

    Add images of socket types

---
+++ logic_nodes/_introduction/make_socket_svgs.py
@@ -0,0 +1,30 @@
+"""This script creates .svg images of all socket types in COLORS."""
+
+COLORS = {
+    "action": "cc4c4c",
+    "animation": "cccccc",
+    "array": "cc6600",
+    "bool": "cca6d6",
+    "color": "c7c729",
+    "dynamic": "63c763",
+    "float": "a1a1a1",
+    "integer": "0f8526",
+    "material": "63c763",
+    "object": "268cbf",
+    "string": "70b2ff",
+    "transform": "63c763",
+    "vector": "6363c7"
+}
+
+
+def main():
+    with open("sockettype_template.svg", "r") as template:
+        template_str = template.read()
+
+    for socket_type, color in COLORS.items():
+        with open(f"sockettype_{socket_type}.svg", "w") as f:
+            f.write(template_str.replace("fill:#cc4c4c;", f"fill:#{color};"))
+
+
+if __name__ == "__main__":
+    main()
